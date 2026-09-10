f561cf07611e60e62f011427f7d1f3925dc48021
gksds123 <seoyul110111@gmail.com>
2026-09-10 18:24:41 +0000

    Fix cache bypass permanently

--- sw.js
+++ sw.js
@@ -1,19 +1,17 @@
-const CACHE_NAME = 'kisb-menu-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './menu.json'
-];
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
 
-self.addEventListener('install', event => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
+  // 캐시를 절대 타지 않고 무조건 서버 네트워크 요청
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    fetch(event.request, { cache: 'no-store' }).catch(() => fetch(event.request))
   );
 });
